87c317b8409bb170bd2a3244f7e8318f761ee2e8
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-22 15:56:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,17 @@
 def using_include(array, element)
-
+  array.include?("element")
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.Reverse
 end
 
 def using_first(array)
-
+  
 end
 
 def using_last(array)
